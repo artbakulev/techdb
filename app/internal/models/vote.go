1de9f44b7a8afb3ee5574b5b1ee5f20dc494e4b4
artbakulev <bakulev.artyom@yandex.ru>
2020-01-16 23:29:53 +0300

    finish with models

--- app/internal/models/vote.go
+++ app/internal/models/vote.go
@@ -1,1 +1,7 @@
 package models
+
+type Vote struct {
+	Nickname string `json:"nickname"`
+	Voice    int8   `json:"voice"`
+	Thread   int32  `json:"thread"`
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/artbakulev/techdb
 
 go 1.13
+
+require github.com/mailru/easyjson v0.7.0
