e73bfb7d4e9807afa180bf11cc57aec81aa43b15
artbakulev <bakulev.artyom@yandex.ru>
2020-01-20 01:02:32 +0300

    service handler

--- app/service/delivery/http/service_handler.go
+++ app/service/delivery/http/service_handler.go
@@ -1,8 +1,10 @@
 package http
 
 import (
+	"github.com/artbakulev/techdb/app/models"
 	"github.com/artbakulev/techdb/app/service"
 	"github.com/buaazp/fasthttprouter"
+	"github.com/valyala/fasthttp"
 )
 
 type ServiceHandler struct {
@@ -10,8 +12,31 @@ type ServiceHandler struct {
 }
 
 func NewServiceHandler(router *fasthttprouter.Router, usecase service.Usecase) {
-	//handler := &ServiceHandler{
-	//	usecase: usecase,
-	//}
-	//router.GET("/api/user/:nickname/profile", handler.GetUser)
+	handler := &ServiceHandler{
+		usecase: usecase,
+	}
+	router.POST("/api/service/clear", handler.Clear)
+	router.GET("/api/service/status", handler.GetStatus)
+}
+
+func (s ServiceHandler) Clear(ctx *fasthttp.RequestCtx) {
+	err := s.usecase.ClearDB()
+	if err != nil {
+		err.SetToContext(ctx)
+	}
+}
+func (s ServiceHandler) GetStatus(ctx *fasthttp.RequestCtx) {
+	dbStatus, e := s.usecase.GetDBStatus()
+	if e != nil {
+		e.SetToContext(ctx)
+		return
+	}
+
+	jsonBlob, err := dbStatus.MarshalJSON()
+	if err != nil {
+		ctx.SetStatusCode(500)
+		ctx.SetBody(models.InternalErrorBytes)
+	}
+
+	ctx.SetBody(jsonBlob)
 }
